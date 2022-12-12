a2f351a47c31bc54c94627ced649eced2080a812
evellyn pereira lacerda matos <evellynl464@gmail.com>
2022-12-12 00:38:32 -0300

    projeto final primeiro módulo

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Soma entre dois números");
+
+Console.WriteLine("Digite um número:");
+string um = Console.ReadLine()!;
+decimal decimalString1 = Convert.ToDecimal(um);
+
+Console.WriteLine("Digite outro número:");
+string dois = Console.ReadLine()!;
+decimal decimalString12 = Convert.ToDecimal(dois);
+
+decimal resultado = decimalString1 + decimalString12;
+
+Console.WriteLine("Seu resultado é:");
+Console.Beep(1320, 1000);
+Console.WriteLine($"{resultado}");
